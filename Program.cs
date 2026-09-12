adba8970277259b5f34c95c178bf31af0d257cd4
IvantsovKirill <krisus0012008@gmail.com>
2026-09-12 19:56:48 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+string name = "Кирилл";
+int age = 18;
+Console.WriteLine("Привет! Меня зовут " + name + " мне " + age + " лет");
+Console.ReadKey();
