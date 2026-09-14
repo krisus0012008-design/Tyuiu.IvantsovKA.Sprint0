409a069ee88000b666ae5a1f643eac49ecbe41ad
IvantsovKirill <krisus0012008@gmail.com>
2026-09-15 02:47:23 +0500

    Add files via upload

---
+++ DataServiceTest.cs
@@ -0,0 +1,30 @@
+using Tyuiu.IvantsovKA.Sprint0.Task5.V0.Lib;
+namespace Tyuiu.IvantsovKA.Sprint0.Task5.V0.Test
+{
+    public class DataServiceTest
+    {
+        [Fact]
+        public void CheckedAdditionValid()
+        {
+            Assert.Equal(10, DataService.Addition(5, 5));
+        }
+
+        [Fact]
+        public void CheckedSubtractionValid()
+        {
+            Assert.Equal(5, DataService.Subtraction(10, 5));
+        }
+
+        [Fact]
+        public void CheckedMultiplicationValid()
+        {
+            Assert.Equal(50, DataService.Multiplication(10, 5));
+        }
+
+        [Fact]
+        public void CheckedDivisionValid()
+        {
+            Assert.Equal(3, DataService.Division(9, 3));
+        }
+    }
+}
